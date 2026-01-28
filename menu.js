defeb08115fdd7f427272e5777ff1d7dbc5cfd9d
tecnoayni <fundacionayninakuna.tecnologia@gmail.com>
2026-01-28 10:09:48 -0400

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,24 +1,26 @@
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 document.addEventListener("DOMContentLoaded", () => {
+    const auth = getAuth();
+
     const btnNuevoRegistro = document.getElementById("btnNuevoRegistro");
     const btnListaRegistros = document.getElementById("btnListaRegistros");
     const btnCerrarSesion = document.getElementById("btnCerrarSesion");
 
-    if (btnNuevoRegistro) {
-        btnNuevoRegistro.addEventListener("click", () => {
-            window.location.href = "NuevoRegistro.html";
-        });
-    }
+    btnNuevoRegistro?.addEventListener("click", () => {
+        window.location.href = "NuevoRegistro.html";
+    });
 
-    if (btnListaRegistros) {
-        btnListaRegistros.addEventListener("click", () => {
-            window.location.href = "Lista.html";
-        });
-    }
+    btnListaRegistros?.addEventListener("click", () => {
+        window.location.href = "Lista.html";
+    });
 
-    if (btnCerrarSesion) {
-        btnCerrarSesion.addEventListener("click", () => {
-            window.location.href = "Index.html"; 
+    btnCerrarSesion?.addEventListener("click", () => {
+        signOut(auth).then(() => {
+            window.location.href = "index.html";
+        }).catch((error) => {
+            alert("Error al cerrar sesión");
+            console.error(error);
         });
-    }
+    });
 });
-
